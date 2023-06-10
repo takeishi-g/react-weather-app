be0c1fee28bae946c3dc33b35b681deb2319e231
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2023-06-11 01:01:13 +0900

    add Results components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-import Title from './components/Title';
-import Form from './components/From';
-import './App.css';
+import { useState } from "react";
+import Title from "./components/Title";
+import Form from "./components/From";
+import Results from "./components/Results";
+import "./App.css";
+
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+};
 
 function App() {
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
+  const getWeather = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=d5e39515a0004593ba0113615230906&q=${city}&aqi=no`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        });
+      });
+  };
   return (
-    <div className="test">
-     <Title />
-     <Form />
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
   );
 }
--- src/components/From.tsx
+++ src/components/From.tsx
@@ -1,21 +1,14 @@
-import { useState } from "react"
+type FormPopsType ={
+  setCity: React.Dispatch<React.SetStateAction<string>>
+  getWeather: (e: React.FormEvent<HTMLFormElement>) => void
+}
 
-
-
-const Form = () => {
-  const [ sity, setSity ] = useState<string>("")
-  const getWeather = async (e:any) => {
-    e.preventDefault()
-    await fetch("https://api.weatherapi.com/v1/current.json?key=d5e39515a0004593ba0113615230906&q=London&aqi=no"
-    )
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }
+const Form = (props:FormPopsType) => {
+  
   return (
-    <form>
-      <input type="text" name="sity" placeholder="都市名" onChange={(e) => setSity(e.target.value)}/>
-      {sity}
-      <button type="submit" onClick={getWeather}>Get Weater</button>
+    <form onSubmit={props.getWeather}>
+      <input type="text" name="sity" placeholder="都市名" onChange={(e) => props.setCity(e.target.value)}/>
+      
     </form>
   )
 }
